0aa399818a4cf9222d831980c1087286efa5b5b6
Dante Del Favero <dante.delfavero@gmail.com>
2017-01-06 19:14:39 +0100

    supporto .net

--- src/app/controllers/mainControllers.js
+++ src/app/controllers/mainControllers.js
@@ -10,7 +10,7 @@ angular.module('mainControllers', [
     .directive("homeFooter", function() {
         return {
             restrict: 'E',
-            templateUrl: "/app/views/shared/footer.html"
+            templateUrl: "../../app/views/shared/footer.html"
         }
     });
 
